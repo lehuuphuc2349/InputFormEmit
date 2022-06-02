82b50ddca0e0173d6628c082c1a2dc8d48c93c18
PhucLH <hphuc23499@gmail.com>
2022-06-02 17:14:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import AdInputVue from "./components/atoms/AdInput.vue";
+import { reactive } from "vue";
+
+const data = reactive({
+  inputVal: "",
+});
+</script>
+
+<template>
+  <AdInputVue
+    placeholder="Please enter your username"
+    v-model="data.inputVal"
+  />
+  <p>Data of input is {{ data.inputVal }}</p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/atoms/AdInput.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+
+type InputTypes =
+  | "text"
+  | "email"
+  | "password"
+  | "radio"
+  | "checkbox"
+  | undefined;
+
+defineProps({
+  type: {
+    type: String as PropType<InputTypes>,
+    required: false,
+    default: "text",
+    validator: (value: string) => {
+      const inputTypes = ["text", "email", "password", "radio", "checkbox"];
+      return inputTypes.includes(value);
+    },
+  },
+  id: {
+    type: String,
+    required: false,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  placeholder: {
+    type: String,
+    required: false,
+  },
+  classes: {
+    type: String,
+    required: false,
+  },
+  modelValue: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <input
+    :type="type"
+    :id="id"
+    :name="name"
+    :placeholder="placeholder"
+    :class="classes"
+    :value="modelValue"
+    @input="$emit('update:modelValue', ($event?.target as HTMLInputElement).value)"
+  />
+</template>
